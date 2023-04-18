b792e5e4bfcda97c5fdb2008253d8316e1aaa547
Caleb Pedro <click2bundi@gmail.com>
2023-04-18 17:39:13 +0100

    server configuration

--- main.go
+++ main.go
@@ -3,12 +3,48 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func helloWorldPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprint(w, "Hello World")
+	case "/ninja":
+		fmt.Fprint(w, "Hello Caleb")
+	default:
+		fmt.Fprint(w, "Big fat error")
+	}
+	fmt.Printf("Handling function with %s request\n", r.Method)
+}
+
+func htmlVsPlain(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Hello World</h1>")
+}
+
+func timeout(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Timeout Attempted")
+	time.Sleep(2 * time.Second)
+	fmt.Fprint(w, "<h1>Hello World</h1>")
+}
+
+func muskRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello from new musk")
 }
 func main() {
-	http.HandleFunc("/", helloWorldPage)
-	http.ListenAndServe("", nil)
+	http.HandleFunc("/", htmlVsPlain)
+	http.HandleFunc("/timeout", timeout)
+	//http.ListenAndServe("", nil)
+
+	server := http.Server{
+		Addr:         "",
+		Handler:      nil,
+		ReadTimeout:  1000,
+		WriteTimeout: 1000,
+	}
+	var newMux http.ServeMux
+	server.Handler = &newMux
+	newMux.HandleFunc("/", muskRoute)
+	server.ListenAndServe()
 }
